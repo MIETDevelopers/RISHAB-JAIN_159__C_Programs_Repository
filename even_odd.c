9976d28d25664f71777009d71f6a58c31b65e255
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-03-19 09:01:59 +0530

    Update even_odd.c

--- even_odd.c
+++ even_odd.c
@@ -1,7 +1,11 @@
+//to check whether a number is even or odd.
+// i have used if else case here but there are many ways to do this 
+
+
 #include <stdio.h>
 
 int main() {
-   int even = 24;
+   int even = 24;             //these are the input from the user.
    int odd  = 31;
    
    if (even % 2 == 0)
