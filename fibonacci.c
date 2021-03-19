f770042a0c5588d6fbe366096571da1b6374e148
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-03-19 09:06:30 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,10 +1,16 @@
+//a simple c program to demonstrate fibonacci series.
+
+
 #include <stdio.h>
 int main() {
-    int i, n, t1 = 0, t2 = 1, nextTerm;
+    int i, n, t1 = 0, t2 = 1, nextTerm;              //used foer the ease of apllication of formula of the series.
     printf("Enter the number of terms: ");
-    scanf("%d", &n);
+    scanf("%d", &n);                                //this is used to read the numbers input by the user.
     printf("Fibonacci Series: ");
 
+    //iam using for loop here as i understood it better.
+    //formula for the series.
+    
     for (i = 1; i <= n; ++i) {
         printf("%d, ", t1);
         nextTerm = t1 + t2;
