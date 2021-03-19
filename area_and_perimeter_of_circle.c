9f9bf035df4c7c51fef2d7effff8a0fb193dc3e7
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-03-19 08:49:51 +0530

    Update area_and_perimeter_of_circle.c

--- area_and_perimeter_of_circle.c
+++ area_and_perimeter_of_circle.c
@@ -1,13 +1,15 @@
+//it is simple c program to calculate area and perimeter of a cirlce.
+
 #include <stdio.h> 
 int main() {
    int radius;
    float area, perimeter;    
    radius = 6;
 
-   perimeter = 2*3.14*radius;
+   perimeter = 2*3.14*radius;   //formula for perimeter of cirlce.
    printf("Perimeter of the Circle = %f inches\n", perimeter);
 	
-	area = 3.14*radius*radius;
+	area = 3.14*radius*radius;    //formula for area of circle.
 	printf("Area of the Circle = %f square inches\n", area);
 
 return(0);
