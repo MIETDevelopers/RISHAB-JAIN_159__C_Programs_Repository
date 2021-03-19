35b9e9eee80205cc94e603ddb01ae1cbfd78291f
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-03-19 09:09:21 +0530

    Update prime_or_not.c

--- prime_or_not.c
+++ prime_or_not.c
@@ -1,13 +1,18 @@
+//it is a simple c program to check whether number is prim or not
+//iam using do whil loop here.
+//iam also using if else case here.
+
+
 #include <stdio.h>
 int main()
 {
    int a, i, c=0;
- 	printf("Enter number to check prime number or not");
-    scanf("%d",&a);
+ 	printf("Enter number to  prime number or not");
+    scanf("%d",&a);                               //to scan input numbers from trhe user
     i=2;
     while( i<=a/2)
     {
-        // check for non prime number
+        //  for non prime number
         if(a%i==0)
         {
             c=1;
