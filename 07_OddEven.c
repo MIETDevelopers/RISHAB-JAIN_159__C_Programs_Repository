2a61cd9cca69e0b1af190f8004c19c51b8b75ef1
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-04-08 18:52:32 +0530

    Update 07_OddEven.c

--- 07_OddEven.c
+++ 07_OddEven.c
@@ -1,10 +1,10 @@
-// A C Program to check whether the entered number is Odd or Even.
+// A simple C Program to  whether the entered number is Odd or Even.
 #include <stdio.h>
 int main() {
     int num;
     printf("Enter an integer: ");
-    scanf("%d", &num); //It will scan the numbers entered by user.
-    if(num % 2 == 0)
+    scanf("%d", &num); //It will scan the values entered by the user 
+    if(num % 2 == 0)                                                    // using if else case here because ease of accessibility
         printf("%d is even.", num);
     else
         printf("%d is odd.", num);
