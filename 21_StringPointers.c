4cb9fa436eb8a0866d1e7f3d177348b4a20805fc
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-04-08 09:48:22 +0530

    Update 21_StringPointers.c

--- 21_StringPointers.c
+++ 21_StringPointers.c
@@ -1,4 +1,4 @@
-//A C Program to read the string in pointers loaction and give it as output.(Pre-Defined) 			© Ishav Verma 19/March/2021
+//A C Program to read the string in pointers loaction and give it as output.(Pre-Defined) 			
 #include <stdio.h>
 
 int main(void) {
