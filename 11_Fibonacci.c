4793bf92f033f939bbbbbe45548adeee98e07d80
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-04-08 18:58:38 +0530

    Update 11_Fibonacci.c

--- 11_Fibonacci.c
+++ 11_Fibonacci.c
@@ -1,14 +1,19 @@
+// a simple c program to print fibonacci series
+
+
+
+
 #include<stdio.h>
  
 int main(){
-   int n, first = 0, second = 1, next, c; //For the reference to the formuls of the series.
+   int n, first = 0, second = 1, next, c; // recommeneded initials for the formula
  
    printf("Enter the number of terms\n");
-   scanf("%d",&n); //This will scan (read) the number entered by user.
+   scanf("%d",&n); //This will scan the total number of terms that are to be printed 
  
    printf("First %d terms of Fibonacci series are :-\n",n);
  
-   for ( c = 0 ; c < n ; c++ ) //ForLoop function for the reference to the formuls of the series.
+   for ( c = 0 ; c < n ; c++ ) //ForLoop function for the reference to the formula of the series.
    {
       if ( c <= 1 )
          next = c;
