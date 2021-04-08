6828e3bb991c45d64ef2f28001f4117fc6e59496
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-04-08 18:46:32 +0530

    Update 04_Arithmetic.c

--- 04_Arithmetic.c
+++ 04_Arithmetic.c
@@ -1,27 +1,32 @@
+// a simple c program to perform various arithmetic operatiions
+// iam using switch case because of easy usability of code but it can be executed using various others type of code
+
+
+
 #include<stdio.h>
 int main(){
     char operator;
     double a, b;
-    printf("Enter an operator (+, -, *, /):"); //It will print the operater to be entered by user.
-    scanf("%c", &operator); //It will scan the operater entered by user.
+    printf("Enter an operator (+, -, *, /):"); //it will print the arithmetic operator entered by the user 
+    scanf("%c", &operator); //It will scan the arithmetic operator entered by the user
     printf("Enter two operands:");
     scanf("%lf %lf",&a, &b); //It will scan the numbers entered by user.
     switch(operator)
     {
         case '+':
-            printf("%.2lf + %.2lf = %.2lf",a, b, a+b); //Addition Function
+            printf("%.2lf + %.2lf = %.2lf",a, b, a+b); //Addition 
             break;
 
         case '-':
-            printf("%.2lf - %.2lf = %.2lf",a, b, a-b); //Subtraction Function
+            printf("%.2lf - %.2lf = %.2lf",a, b, a-b); //Subtraction
             break;
 
         case '*':
-            printf("%.2lf * %.2lf = %.2lf",a, b, a*b); //Multiplication Function
+            printf("%.2lf * %.2lf = %.2lf",a, b, a*b); //Multiplication
             break;
 
         case '/':
-            printf("%.2lf / %.2lf = %.2lf",a, b, a/b); //Division Function
+            printf("%.2lf / %.2lf = %.2lf",a, b, a/b); //Division 
             break;
     printf("Error encountered by the input values");
     }
