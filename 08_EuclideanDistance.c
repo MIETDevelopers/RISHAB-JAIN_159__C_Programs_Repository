050c201d6c762854c99cc84ef55c1000fd51e0a7
Rishab Jain <75359063+rishaxj@users.noreply.github.com>
2021-04-08 18:54:30 +0530

    Update 08_EuclideanDistance.c

--- 08_EuclideanDistance.c
+++ 08_EuclideanDistance.c
@@ -1,16 +1,16 @@
-//A C Program to calculate distance between two points.
-#include<stdio.h> //Including Standard input & output header file from library. 
-#include<math.h>  //Including maths header file from library/
+//A simple C Program to calculate distance between two points.
+#include<stdio.h> //Including Standard input & output header file 
+#include<.h>  //Including maths header file 
   
 int main()  
 {  
-    float x1, y1, x2, y2, distance; //Declaring variables. 
+    float x1, y1, x2, y2, distance; //Declaring variables that are to be used in the formula
   
     printf("Enter point 1 (x1, y1)\n");  
-    scanf("%f %f", &x1, &y1);  //Scanning coordinates for first point.
+    scanf("%f %f", &x1, &y1);  //Scanning coordinates for first point as provider will provide it
   
     printf("Enter point 2 (x2, y2)\n");  
-    scanf("%f %f", &x2, &y2);  //Scanning coordinates for second point.
+    scanf("%f %f", &x2, &y2);  //Scanning coordinates for second point as provider will provide it
   
     distance = sqrt( (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) ); //Euclidean Distance's formula. 
   
